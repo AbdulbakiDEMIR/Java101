50852cb36c71da53414b968ed2d7efd71524d80b
Abdülbaki Demir <97521562+AbdulbakiDEMIR@users.noreply.github.com>
2022-11-03 18:10:19 +0300

    Alisveris Tutari

---
+++ odev7_alisveris_tutar.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class odev7_alisveris_tutar {
+    public static void main(String[] argv){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ürünlerden nekadar arlmak istediğinizi girin");
+        System.out.print("Armut: ");
+        double armut = input.nextDouble();
+        System.out.print("Elma: ");
+        double elma = input.nextDouble();
+        System.out.print("Domates: ");
+        double domates = input.nextDouble();
+        System.out.print("Muz: ");
+        double muz = input.nextDouble();
+        System.out.print("Patlıcan: ");
+        double patlican = input.nextDouble();
+        double tutar = (armut*2.14)+(elma*3.67)+(domates*1.11)+(muz*0.95)+(patlican*5);
+        System.out.println("Tutar: "+tutar);
+    }
+}
